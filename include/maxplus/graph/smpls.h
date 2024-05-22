afad1103f95bfd3acfeabe2de8f9c9075e037541
Marc Geilen <m.c.w.geilen@tue.nl>
2024-05-22 21:09:49 +0200

    another missing header guard

--- include/maxplus/graph/smpls.h
+++ include/maxplus/graph/smpls.h
@@ -1,3 +1,47 @@
+/*
+ *  Eindhoven University of Technology
+ *  Eindhoven, The Netherlands
+ *  Dept. of Electrical Engineering
+ *  Electronics Systems Group
+ *  Model Based Design Lab (https://computationalmodeling.info/)
+ *
+ *  Name            :   smpls.h
+ *
+ *  Author          :   Marc Geilen (<m.c.w.geilen@tue.nl>)
+ *                  :   Alireza Mohammadkhani (<a.mohammadkhani@tue.nl>)
+ *
+ *  Date            :   April 26, 2010
+ *
+ *  Function        :   Switched Max-plus Linear Systems.
+ *
+ *  History         :
+ *      22-05-24    :   Initial version copied over from SDF3
+ *
+ *
+ *  Copyright 2023 Eindhoven University of Technology
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the “Software”),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#ifndef MAXPLUS_GRAPH_SMPLS_H
+#define MAXPLUS_GRAPH_SMPLS_H
+
 #include "maxplus/algebra/mpmatrix.h"
 #include "maxplus/base/fsm/fsm.h"
 #include "maxplus/base/fsm/iofsm.h"
@@ -144,3 +188,5 @@ private:
 };
 
 } // namespace MaxPlus
+
+#endif
